b60cc1e512783e9a341f3c92910ab8119a6afe98
Adam Gray <adam@quixey.com>
2014-12-04 10:32:57 -0800

    Fix test setup a bit, use setup.cfg

--- Makefile
+++ Makefile
@@ -33,7 +33,7 @@ virtualenv:
 	rm -rf python-aliyun.egg-info
 
 test: clean
-	python ./setup.py nosetests --verbosity 2 --tests tests/unit --with-coverage --cover-package aliyun --cover-xml --cover-xml-file=build/coverage.xml --with-xunit --xunit-file=build/xunit-results.xml
+	python ./setup.py nosetests
 	mv *.egg-info build/
 	rm .coverage
 
--- tests/functional/readonly.py
+++ tests/functional/readonly.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 # Copyright 2014, Quixey Inc.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in self.c.mpliance with the License. You may obtain a copy of
 # the License at
-# 
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -71,4 +71,4 @@ class EcsReadOnlyTest(unittest.TestCase):
         print self.c.describe_auto_snapshot_policy()
 
 if __name__ == '__main__':
-    unittests.main()
+    unittest.main()
